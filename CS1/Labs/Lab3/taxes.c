4f19b7eb54349e7359e721fbacfe118d2f351b0d
AbdullahKhanSwati <101318760+AbdullahKhanSwati@users.noreply.github.com>
2023-10-22 10:32:59 +0500

    added programe taxes.c

---
+++ CS1/Labs/Lab3/taxes.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  double agi = 0.0;
+  char c = 'N';
+  double tax = 0.0;
+  double childCredit = 0.0;
+  double totalTax = 0.0;
+  int numChildren = 0;
+
+  printf("Please enter your adjusted gross income (AGI): ");
+  scanf("%lf", &agi);
+  
+  getchar(); 
+
+  printf("Do you have any children? (Y) or (N)? ");
+  c = getchar();
+
+  if(c == 'y' || c == 'Y') {
+    printf("How many children do you have? ");
+    scanf("%d", &numChildren);
+  }
+
+ 
+
+  printf("AGI:          $%10.2f\n", agi);
+  printf("Tax:          $%10.2f\n", tax);
+  printf("Child Credit: $%10.2f\n", childCredit);
+  printf("Total Tax:    $%10.2f\n", totalTax);
+
+  return 0;
+}
